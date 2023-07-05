07cbb9c293baeef31d900b44f4a255987f2cdd7d
deferredexistance <137775601+deferredexistance@users.noreply.github.com>
2023-07-05 11:13:40 +0530

    Create PipelineScript.groovy
    First Version of Jenkins Pipeline Groovy Script

---
+++ PipelineScript.groovy
@@ -0,0 +1,29 @@
+node {
+    
+    stage('Preparation') { // for display purposes
+        // Get some code from a GitHub repository
+        //git 'https://github.com/jglick/simple-maven-project-with-tests.git'
+        // Get the Maven tool.
+        // ** NOTE: This 'M3' Maven tool must be configured
+        // **       in the global configuration.
+        
+    }
+    stage('Perform Model-In-Loop Tests') {
+        // Run the maven build
+        build 'Model_In_Loop_Stage'
+        }
+    
+    stage('Perform Auto CodeGenerattion') {
+        // Run the maven build
+        build 'CodeGen'
+        }
+    
+    stage('Perform Software-In-Loop Tests') {
+        // Run the maven build
+        build 'Software_In_Loop_Stage'
+        }
+    
+    stage('Results') {
+        
+    }
+}

7d3fe2d8d365ee22778d08f92fe3dcf88aff40ef
deferredexistance <rarora@mathworks.com>
2023-07-13 14:47:59 +0530

    Demo 1

--- startup.m
+++ startup.m
@@ -2,12 +2,5 @@ project = currentProject;
 projectRoot = project.RootFolder;
 
 cacheFolder = fullfile(projectRoot, 'work');
-%% Adding this comment to check the MBD pipeline
-%% Test to check the pipeline on AWS
-%% Test 3
-%% Test 12345678888
-%% This is a test change to show a demo of CI/CD pipleine
-%% Another test change 1234
-%% Test for CI/CD
-%% Test 1234
-%% Test 12345124324321
+%%Demo
+
